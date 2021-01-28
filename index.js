b498723e9ed79f3cd2ece81c852bc3bd3483d70e
XMSBOT <78173635+XMSBOT@users.noreply.github.com>
2021-01-28 18:13:12 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const qrcode = require("qrcode-terminal");
+const menu = require("./menu.js");
+const fs = require("fs");
+const moment = require("moment");
+const imageToBase64 = require("image-to-base64");
+const axios = require("axios");
+const
+{
+   WAConnection,
+   MessageType,
+   Presence,
+   MessageOptions,
+   Mimetype,
+   WALocationMessage,
+   WA_MESSAGE_STUB_TYPES,
+   ReconnectMode,
+   ProxyAgent,
+   waChatKey,
+} = require("@adiwajshing/baileys");
+const { count } = require("console");
+var jam = moment().format("HH:mm");
+
+function foreach(arr, func)
+{
+   for (var i in arr)
+   {
+      func(i, arr[i]);
+   }
+}
+const conn = new WAConnection()
+conn.on('qr', qr =>
+{
+   qrcode.generate(qr,
+   {
+      small: true
+   });
+   console.log(`[ ${moment().format("HH:mm:ss")} ] Por favor escaneie o QR-Code com seu aplicativo!`);
+});
